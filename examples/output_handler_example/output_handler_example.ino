b42e429cc155a96d194d68aaa2687d5d648164c9
Eric Z. Ayers <ericzundel@gmail.com>
2023-05-09 11:15:02 -0400

    Updated example code.

--- examples/output_handler_example/output_handler_example.ino
+++ examples/output_handler_example/output_handler_example.ino
@@ -1,78 +1,105 @@
- 
-#include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+/*
+ * output_handler_example - Example file for using the PinballOutputHandler library.
+ * Author: Eric Z. Ayers <ericzundel@gmail.com>
+ * 
+ * The library weaps the AdafruitNeopixel library in order to perform effects
+ * in the background without calling delay() to block your program.
+ * This works by keeping state in the handler objects and then activating a
+ * state machine for each handler when handleAll() is called in the main loop.
+ * 
+ * See https://github.com/ericzundel/pinball_output_handler
+ */
+#include <PinballOutputHandler.h>
 
-#include <OutputHandler.h>
-#include <NeoPixelColor.h>
-#include <NeoPixelHandler.h>
- 
+// This object holds all of your background handlers.  To execute the handlers, call outputHandler.handleAll()
+// in loop().
 OutputHandler outputHandler;
-NeoPixelHandler *handlers[6]; // to hold handlers that can control 6 strips
+
+// Initialize all the neopixel strips and save them in an array so you can
+// change values like brightness later.
+#define NUM_STRIPS 6
+
+Adafruit_NeoPixel pixels[NUM_STRIPS] = {
+  // Initialize the strips with the number of LEDS, pin number, and init parameters.
+  // Replace the pin numbers with the pins you are using for your neopixel strips
+  Adafruit_NeoPixel(10, 17, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(10, 19, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(10, 21, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(10, 22, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(10, 26, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(10, 28, NEO_GRB + NEO_KHZ800)
+};
+
+// An array to hold output handlers that correspond to the strips
+NeoPixelHandler *handlers[NUM_STRIPS]; // to hold handlers that can control 6 strips
+
+// Define custom colors here
+NeoPixelColor cyan(0, 255, 255);
 
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  Serial.println("Initializing in 5 seconds");
-  delay(5000);
-  Serial.println("Initializing");
-  Serial.println("Send a number to simulate a button");
-  
-  // Add six handlers that have 10 LEDs each on different output pins of the Arduino
-  handlers[0] = outputHandler.add(*(new Adafruit_NeoPixel(10, 17, NEO_GRB + NEO_KHZ800)));
-  handlers[1] = outputHandler.add(*(new Adafruit_NeoPixel(10, 19, NEO_GRB + NEO_KHZ800)));
-  handlers[2] = outputHandler.add(*(new Adafruit_NeoPixel(10, 21, NEO_GRB + NEO_KHZ800)));
-  handlers[3] = outputHandler.add(*(new Adafruit_NeoPixel(10, 22, NEO_GRB + NEO_KHZ800)));
-  handlers[4] = outputHandler.add(*(new Adafruit_NeoPixel(10, 26, NEO_GRB + NEO_KHZ800)));
-  handlers[5] = outputHandler.add(*(new Adafruit_NeoPixel(10, 28, NEO_GRB + NEO_KHZ800)));
-}
+  Serial.println("Send in the Serial Monitor a number to simulate a button");
+
+  // Initialize the output handler with the neopixel strip objects. Save the handler objects for later.
+  for (int i = 0; i < NUM_STRIPS; i++) {
+    handlers[i] = outputHandler.add(pixels[i]); // Note, this library calls begin() on the Adafruit_NeoPixel library.
+    pixels[i].setBrightness(25); // Set brightness to 10% to save power.
+  }
+} // end setup()
 
 void loop() {
- //debug("loop");
+  //debug("loop");
 
- // I don't have buttons on my test board, I'm simulating by pressing keys on the keyboard
- if (Serial.available()) {
-    // read the incoming byte
+  // I don't have buttons on my test board, I'm simulating by sending numbers over the Serial Monitor.
+  if (Serial.available()) {
     char incomingByte = Serial.read();
-    switch (incomingByte) {
-      case '0':
-         // Blink strip 0
-         Serial.println("Blinking Strip 0");
-         handlers[0]->blink(NeoPixelColor::GREEN, 100, 10);
-         break;
-         
-     case '1':
-         // Blink strip 1
-         Serial.println("Blinking Strip 1");
-         handlers[1]->blink(NeoPixelColor::RED, 250, 5);
-         break;
-         
-     case '2':
-         // Blink strip 2
-         Serial.println("Blinking Strip 2");
-         handlers[2]->blink(NeoPixelColor::PINK, 200, 20);
-         break;    
 
-     case '3':
-         // Blink strip 3
-         Serial.println("Blinking Strip 3");
-         handlers[3]->blink(NeoPixelColor::WHITE, 100, 30);
-         break;
-
-     case '4':
-         // Blink strip 4
-         Serial.println("Blinking Strip 4");
-         handlers[4]->blink(NeoPixelColor::MAGENTA, 1000, 10);
-         break;    
-
-     case '5':
-         // Blink strip 5
-         Serial.println("Blinking Strip 5");
-         handlers[5]->blink(NeoPixelColor::YELLOW, 1000, 10);
-         break;     
-     default:
-         Serial.print("Didn't understand input: ");
-         Serial.println(incomingByte);
+    Serial.print("Got input: ");
+    Serial.println(incomingByte);
+    if (incomingByte == '0') {
+      Serial.println("Blinking Strip 0 with custom color CYAN");
+      // Call the handler to start the blink. The actuall blinking is handled when you call handleAll();
+      // blink (color, delay in milliseconds, number of times to repeat)
+      handlers[0]->blink(cyan, 100, 40);
+    } else if (incomingByte == '1') {
+      Serial.println("Blinking Strip 1 RED");
+      handlers[1]->blink(NeoPixelColor::RED, 250, 5);
+    } else if (incomingByte == '2') {
+      Serial.println("Blinking Strip 2 PINK");
+      handlers[2]->blink(NeoPixelColor::PINK, 200, 20);
+    } else if (incomingByte == '3') {
+      Serial.println("Blinking Strip 3 GREEN");
+      handlers[3]->blink(NeoPixelColor::GREEN, 100, 30);
+    } else if (incomingByte == '4') {
+      Serial.println("Setting strip 0 to MAGENTA");
+      handlers[0]->on(NeoPixelColor::MAGENTA);
+    } else if (incomingByte == '5') {
+      Serial.println("Turning off strip 0");
+      handlers[0]->off();
+    } else if (incomingByte == '6') {
+      Serial.println("Chasing strip 0 up in YELLOW");
+      // chase_up(color, delay in milliseconds, number of times to repeat)
+      handlers[0]->chase_up(NeoPixelColor::YELLOW, 50, 6);
+    } else if (incomingByte == '7') {
+      Serial.println("Chasing strip 0 down in BLUE");
+      // chase_down(color, delay in milliseconds, number of times to repeat)
+      handlers[0]->chase_down(NeoPixelColor::BLUE, 100, 5);
+    } else if (incomingByte == '8') {
+      Serial.println("Rainbow cycle strip 0");
+      // rainbow_cycle(delay in milliseconds, number of times to repeat
+      handlers[0]->rainbow_cycle(1000, 4);
+    } else if (incomingByte == '9') {
+      Serial.println("Rainbow strip 0");
+      // make the strip light up in rainbow colors
+      handlers[0]->rainbow();
+    } else if (incomingByte == '\n') {
+      // ignore newlines
+    } else {
+      Serial.print("Didn't understand input: ");
+      Serial.println(incomingByte);
     }
- }
- outputHandler.handleAll();
-}
+  }
+
+  // Call the output Handler every time you go through the main loop.
+  outputHandler.handleAll();
+} // end loop()
